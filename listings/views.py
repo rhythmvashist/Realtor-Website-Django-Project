7482ba1f3da3702d81dd9f330767be8fa1bbc4ab
Rhythmvashist <vashistrhythm1@gmail.com>
2019-01-01 21:42:04 -0800

    made about page dynamic with db

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 # Create your views here.
 
@@ -6,10 +7,14 @@ from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)
+
+    paginator=Paginator(listings,3)
+    page=request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings':listings
+        'listings':paged_listings
     }
 
     return render(request,'listings/listings.html',context)
