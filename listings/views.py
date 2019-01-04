63daf63800bef1c23cb0aae36008ad993f12fc03
Rhythmvashist <vashistrhythm1@gmail.com>
2019-01-04 10:02:01 -0800

    single listing done

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from .choices import price_choices,state_choices,bedroom_choices
+
 
 # Create your views here.
 
@@ -20,7 +22,18 @@ def index(request):
     return render(request,'listings/listings.html',context)
 
 def listing(request,listing_id):
-    return render(request,'listings/listing.html')
+
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request,'listings/listing.html',context)
 
 def search(request):
+    context = {
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices
+    }
     return render(request,'listings/search.html')
